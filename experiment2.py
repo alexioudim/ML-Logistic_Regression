b129358fbae1e0caa94b2093c4c156622eb63c92
Alexiou Dimitrios <it2021002@hua.gr>
2024-12-01 17:59:18 +0200

    Changes in experiment2.py

--- experiment2.py
+++ experiment2.py
@@ -4,12 +4,16 @@ from sklearn import metrics
 from sklearn import preprocessing
 import numpy as np
 import logistic_regression as lr
+import time
+import platform
+import psutil
 
 # Load the dataset
 data = datasets.load_breast_cancer()
 X, y = data.data, data.target
-mean_accuracy = 0
+accuracies = []
 
+start_time = time.time()
 for _ in range(20):
     # Split the dataset
     x_train, x_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.3)
@@ -28,8 +32,17 @@ for _ in range(20):
     # Test the model's effectiveness
     y_pred = model.predict(x_test) >= 0.5
     accuracy = metrics.accuracy_score(y_test, y_pred)
-    mean_accuracy += accuracy
+    accuracies.append(accuracy)
     print (f"Accuracy: {accuracy}")
 
-print(f"Mean Accuracy: {mean_accuracy/20}")
+end_time = time.time()
 
+mean_accuracy = np.mean(accuracies)
+std_accuracy = np.std(accuracies)
+
+execution_time = end_time - start_time
+
+# Print results
+print(f"Mean accuracy: {mean_accuracy}")
+print(f"Standard deviation of accuracy: {std_accuracy}")
+print(f"Execution time: {execution_time}")
